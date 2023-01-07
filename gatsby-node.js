55bfd5d4439adfd5c46fd776b050b9488ee6f022
Ki-Young <kykim8180@gmail.com>
2023-01-07 23:45:19 +0900

    -ing : set project config
    - eslint, prettier, typescript 설정
    - npm -> yarn package-lock.json 파일 제거 - yarn.lock 으로 대체
    - emotion 설치

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,31 +1,12 @@
 /**
  * Implement Gatsby's Node APIs in this file.
  *
- * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-node/
- */
-
-/**
- * @type {import('gatsby').GatsbyNode['createPages']}
- */
-exports.createPages = async ({ actions }) => {
-  const { createPage } = actions
-  createPage({
-    path: "/using-dsg",
-    component: require.resolve("./src/templates/using-dsg.js"),
-    context: {},
-    defer: true,
-  })
-}
-
-/**
- * Implement Gatsby's Node APIs in this file.
- *
  * See: https://www.gatsbyjs.com/docs/node-apis/
  */
 
 // You can delete this file if you're not using it
 
-const path = require("path")
+const path = require('path')
 
 // Setup Import Alias
 exports.onCreateWebpackConfig = ({ getConfig, actions }) => {
@@ -34,10 +15,11 @@ exports.onCreateWebpackConfig = ({ getConfig, actions }) => {
   actions.setWebpackConfig({
     output,
     resolve: {
+      // 이부분을 통해 매핑을 연결해줌
       alias: {
-        components: path.resolve(__dirname, "src/components"),
-        utils: path.resolve(__dirname, "src/utils"),
-        hooks: path.resolve(__dirname, "src/hooks"),
+        components: path.resolve(__dirname, 'src/components'),
+        utils: path.resolve(__dirname, 'src/utils'),
+        hooks: path.resolve(__dirname, 'src/hooks'),
       },
     },
   })
