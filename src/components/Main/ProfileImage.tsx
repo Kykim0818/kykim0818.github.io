9af6a191f482f75903687e2282309d77d697a276
Ki-Young <kykim8180@gmail.com>
2023-03-25 19:33:11 +0900

    add images

--- src/components/Main/ProfileImage.tsx
+++ src/components/Main/ProfileImage.tsx
@@ -3,7 +3,7 @@ import styled from '@emotion/styled'
 
 // 자신이 원하는 프로필 이미지 링크로 설정해주세요.
 const PROFILE_IMAGE_LINK =
-  'https://avatars.githubusercontent.com/u/24629040?s=460&u=0bb3411f25c0e1c5d25d753fc648739367cb7032&v=4'
+  'https://github.com/Kykim0818/kykim0818.github.io/blob/dev/src/images/Profile1.jpg'
 
 const ProfileImageWrapper = styled.img`
   width: 120px;
