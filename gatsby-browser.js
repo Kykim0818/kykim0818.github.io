675d3df646f31ba2d26fb5b775f64d04d6b81c7f
Ki-Young <kykim8180@gmail.com>
2023-04-30 14:39:26 +0900

    prism-tomorrow.css import

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,3 +5,7 @@
  */
 
 // You can delete this file if you're not using it
+
+import 'prismjs/themes/prism-tomorrow.css'
+// https://github.com/PrismJS/prism/tree/1d5047df37aacc900f8270b1c6215028f6988eb1/themes
+// 테마 다른거 적용 가능
