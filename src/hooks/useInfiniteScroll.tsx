4639757e0e45f9b221d982757156317887d66912
Ki-Young <kykim8180@gmail.com>
2023-09-10 22:16:25 +0900

    사이트맵 추가 및 빌드시에 문제 해결

--- src/hooks/useInfiniteScroll.tsx
+++ src/hooks/useInfiniteScroll.tsx
@@ -13,6 +13,8 @@ const useInfiniteScroll = function (
   posts: PostListItemType[],
 ): useInfiniteScrollType {
   const containerRef = useRef<HTMLDivElement>(null)
+  const observer: MutableRefObject<IntersectionObserver | null> =
+    useRef<IntersectionObserver>(null)
   const [count, setCount] = useState(1)
 
   const postListByCategory = useMemo<PostListItemType[]>(
@@ -29,25 +31,26 @@ const useInfiniteScroll = function (
       ),
     [selectedCategory],
   )
-
-  const observer: IntersectionObserver = new IntersectionObserver(
-    (entries, observer) => {
+  useEffect(() => {
+    observer.current = new IntersectionObserver((entries, observer) => {
       if (entries[0].isIntersecting === false) return
       setCount(value => value + 1)
-      observer.disconnect()
-    },
-  )
+      // 관측중인 객체만 관측을 중지하도록 함
+      observer.unobserve(entries[0].target)
+    })
+  }, [])
 
   useEffect(() => setCount(1), [selectedCategory])
   useEffect(() => {
     if (
       NUMER_OF_ITEMS_PER_PAGE * count >= postListByCategory.length ||
       containerRef.current === null ||
-      containerRef.current.children.length === 0
+      containerRef.current.children.length === 0 ||
+      observer.current === null
     )
       return
 
-    observer.observe(
+    observer.current.observe(
       containerRef.current.children[containerRef.current.children.length - 1],
     )
   }, [count, selectedCategory])
